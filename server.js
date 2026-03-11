76f4fce90b1e25c0fa5ff9175539c86294946192
arpithashivakumar17 <82916921+arpithashivakumar17@users.noreply.github.com>
2026-03-11 14:02:24 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 
+const express = require('express')
+const app = express()
+
+const macarons = require('./macarons.json')
+
+app.use(express.static('public'))
+
+app.get('/api/macarons', (req,res)=>{
+    res.json(macarons)
+})
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+})
